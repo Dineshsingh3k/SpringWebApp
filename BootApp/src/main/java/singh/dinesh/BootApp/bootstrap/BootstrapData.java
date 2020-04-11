e10309e6ae8836b1021dfd560a768935a98ea3d4
Dinesh singh <dinesh.singh3k@gmail.com>
2020-04-11 14:49:04 +0530

    BootstrapData

--- BootApp/src/main/java/singh/dinesh/BootApp/bootstrap/BootstrapData.java
+++ BootApp/src/main/java/singh/dinesh/BootApp/bootstrap/BootstrapData.java
@@ -1,11 +1,41 @@
 package singh.dinesh.BootApp.bootstrap;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import singh.dinesh.BootApp.domain.Author;
+import singh.dinesh.BootApp.domain.Book;
+import singh.dinesh.BootApp.repository.AuthorRepository;
+import singh.dinesh.BootApp.repository.BookRepository;
 
+@Component
 public class BootstrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
 
     @Override
     public void run(String... args) throws Exception {
+        Author dinesh = new Author("Dinesh", "Singh");
+        Book book1 =  new Book("MyBook", "111111111111111");
+        dinesh.getBooks().add(book1);
+        book1.getAuthors().add(dinesh);
 
+        authorRepository.save(dinesh);
+        bookRepository.save(book1);
+
+        Author shreya = new Author("Shreya", "Singh");
+        Book book2 =  new Book("MyCookeryBook", "2222222222222");
+        shreya.getBooks().add(book2);
+        book2.getAuthors().add(shreya);
+
+        authorRepository.save(shreya);
+        bookRepository.save(book2);
+
+        System.out.println("Total Books" + bookRepository.count());
     }
 }
--- BootApp/src/main/java/singh/dinesh/BootApp/domain/Book.java
+++ BootApp/src/main/java/singh/dinesh/BootApp/domain/Book.java
@@ -1,4 +1,92 @@
 package singh.dinesh.BootApp.domain;
 
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private Long Id;
+
+    private String title;
+    private String isbn;
+
+    @ManyToMany
+    @JoinTable (name = "author_book" , joinColumns = @JoinColumn(name = "book_Id"),
+                inverseJoinColumns = @JoinColumn(name = "author_Id"))
+    private Set<Author> authors = new HashSet<>();
+
+    public Book() {
+    }
+
+    public Book(String title, String isbn) {
+        this.title = title;
+        this.isbn = isbn;
+    }
+
+
+    public Book(String title, String isbn, Set<Author> authors) {
+        this.title = title;
+        this.isbn = isbn;
+        this.authors = authors;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(Id, book.Id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Id);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "Id=" + Id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
 }
