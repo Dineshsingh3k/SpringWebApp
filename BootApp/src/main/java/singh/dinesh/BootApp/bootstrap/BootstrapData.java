7da38426c7b5c3e828dc08a910a3e1d6f4413037
Dinesh singh <dinesh.singh3k@gmail.com>
2020-04-11 15:54:04 +0530

    Added PUblisher relationship

--- BootApp/src/main/java/singh/dinesh/BootApp/bootstrap/BootstrapData.java
+++ BootApp/src/main/java/singh/dinesh/BootApp/bootstrap/BootstrapData.java
@@ -24,35 +24,37 @@ public class BootstrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        Publisher tata = new Publisher("tataMcgrawHill", "RoseIcon", "Pune", "Maharashtra", "12312312");
+        publisherRepository.save(tata);
+
         Author dinesh = new Author("Dinesh", "Singh");
         Book book1 =  new Book("MyBook", "111111111111111");
         dinesh.getBooks().add(book1);
         book1.getAuthors().add(dinesh);
+        book1.setPublisher(tata);
+        tata.getBooks().add(book1);
 
         authorRepository.save(dinesh);
         bookRepository.save(book1);
+        publisherRepository.save(tata);
 
         Author shreya = new Author("Shreya", "Singh");
         Book book2 =  new Book("MyCookeryBook", "2222222222222");
         shreya.getBooks().add(book2);
         book2.getAuthors().add(shreya);
+        book2.setPublisher(tata);
+        tata.getBooks().add(book2);
 
         authorRepository.save(shreya);
         bookRepository.save(book2);
-
-        Publisher tata = new Publisher("tataMcgrawHill", "RoseIcon", "Pune", "Maharashtra", "12312312");
         publisherRepository.save(tata);
+
 
         System.out.println("Total Books" + bookRepository.count());
         System.out.println("PUblisher " + publisherRepository.count());
         System.out.println("Author" + authorRepository.count());
+        System.out.println("Books published by tata : " + tata.getBooks().size());
 
-
-
-        /*System.out.println("Dinesh : " + dinesh);
-        System.out.println("Book1 : " + book1);
-        System.out.println("Shreya : " + shreya);
-        System.out.println("Book2 : " + book2);
-        System.out.println("Publisher : " + tata);*/
     }
 }
--- BootApp/src/main/java/singh/dinesh/BootApp/domain/Book.java
+++ BootApp/src/main/java/singh/dinesh/BootApp/domain/Book.java
@@ -15,6 +15,9 @@ public class Book {
     private String title;
     private String isbn;
 
+    @ManyToOne
+    private Publisher publisher;
+
     @ManyToMany
     @JoinTable (name = "author_book" , joinColumns = @JoinColumn(name = "book_Id"),
                 inverseJoinColumns = @JoinColumn(name = "author_Id"))
@@ -33,6 +36,14 @@ public class Book {
         this.title = title;
         this.isbn = isbn;
         this.authors = authors;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     public Long getId() {
